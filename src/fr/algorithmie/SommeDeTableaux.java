86ae33b53acbd5ed8ab45597d840aaae9cf67af8
Julien BURTE <jburte34@gmail.com>
2025-07-08 09:46:14 +0200

    TP 7 modif Somme tableaux

--- src/fr/algorithmie/SommeDeTableaux.java
+++ src/fr/algorithmie/SommeDeTableaux.java
@@ -1,20 +1,18 @@
 package fr.algorithmie;
 
+import java.util.Arrays;
+
 public class SommeDeTableaux {
     public static void main(String[] args) {
         int[] array1 = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
         int[] array2 = {-1, 12, 17, 14, 5, -9, 0, 18, -6, 0, 4, -13, 5, 7, -2, 8, -1};
 
-        int somme = 0;
+        int[] arraySomme = new int[array1.length];
 
-        for (int val : array1) {
-            somme += val;
+        for (int i = 0; i < arraySomme.length; i++) {
+            arraySomme[i] = array1[i] + array2[i];
         }
 
-        for (int val : array2) {
-            somme += val;
-        }
-
-        System.out.println(somme);
+        System.out.println(Arrays.toString(arraySomme));
     }
 }
--- src/fr/algorithmie/SommeDeTableauxDiff.java
+++ src/fr/algorithmie/SommeDeTableauxDiff.java
@@ -1,20 +1,20 @@
 package fr.algorithmie;
 
+import java.util.Arrays;
+
 public class SommeDeTableauxDiff {
     public static void main(String[] args) {
         int[] array1 = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
-        int[] array2 = {-1, 12, 17, 14, 5, -9, 0, 18};
+        int[] array2 = {-1, 12, 17, 14, 5, -9, 0, 18 } ;
 
-        int somme = 0;
+        int[] arraySomme = new int[Math.max(array1.length, array2.length)];
 
-        for (int val : array1) {
-            somme += val;
+        for (int i = 0; i < arraySomme.length; i++) {
+            int val1 = i < array1.length? array1[i]: arraySomme[i];
+            int val2 = i < array2.length? array2[i]: arraySomme[i];
+            arraySomme[i] = val1 + val2;
         }
 
-        for (int val : array2) {
-            somme += val;
-        }
-
-        System.out.println(somme);
+        System.out.println(Arrays.toString(arraySomme));
     }
 }
